a0711620f29b175920e309d869cb85d1a0d4d965
Jamie Brown <jmbrowncow@gmail.com>
2018-08-14 14:05:16 +0000

    automatically backed up by learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@
 # Artist.create(name: "Adele")
 # Artist.create(name: "Florence and the Machine")
 # Artist.create(name: "Muze")
-# 
+#
 # Song.create(name: "Somewhere over the rainbow")
 # Song.create(name: "Hello")
 # Song.create(name: "uprising")
-# 
+#
 # Genre.create(name: "Alternative")
 # Genre.create(name: "Freaky Jive")
 # Genre.create(name: "Jazz")
@@ -14,10 +14,10 @@
 # Artist.new(name: "Adele")
 # Artist.new(name: "Florence and the Machine")
 # Artist.new(name: "Muze")
-# 
+#
 # Song.new(name: "Somewhere over the rainbow")
 # Song.new(name: "Hello")
 # Song.new(name: "uprising")
-# 
+#
 # Genre.new(name: "Alternative")
 LibraryParser.parse
